65ca621b02103d1c82bb97d240bb1f3112b5ae5b
andersonkaiti <anderkaiti@gmail.com>
2026-04-19 20:17:59 -0300

    :sparkles: feat(eslint): initialize eslint configuration

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import js from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.browser
+    }
+  },
+  pluginReact.configs.flat.recommended,
+]);
